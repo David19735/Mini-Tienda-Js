4c30714726f7d7ff14874bdb6282683acd5bbfb0
David19735 <david_19735@hotmail.com>
2024-11-21 19:34:11 -0600

    Proyecto finalizado

--- public/bundle.js
+++ public/bundle.js
@@ -258,3 +258,50 @@ ventanaCarrito.querySelector('#carrito__btn-comprar').addEventListener('click',(
 
 
 });
+
+const contenedorTabs=document.querySelector('.tabs');
+const botones=document.querySelectorAll('.tabs__button');
+const pestañas=document.querySelectorAll('.tab');
+
+
+
+contenedorTabs.addEventListener('click',(e)=>{
+    
+    const tabEnlace=e.target.dataset.tab;
+    
+    
+    if(e.target.closest('button')){
+        if(tabEnlace==="caracteristicas"){
+            e.target.classList.add('tabs__button--active');
+            document.getElementById('caracteristicas').classList.add('tab--active');
+
+
+            botones[1].classList.remove('tabs__button--active');
+            botones[2].classList.remove('tabs__button--active');
+
+            pestañas[1].classList.remove('tab--active');
+            pestañas[2].classList.remove('tab--active');
+        }
+        else if(tabEnlace==="reseñas"){
+            e.target.classList.add('tabs__button--active');
+            document.getElementById('reseñas').classList.add('tab--active');
+
+            botones[0].classList.remove('tabs__button--active');
+            botones[2].classList.remove('tabs__button--active');
+
+            pestañas[0].classList.remove('tab--active');
+            pestañas[2].classList.remove('tab--active');
+        }
+        else if(tabEnlace==="envio"){
+            e.target.classList.add('tabs__button--active');
+            document.getElementById('envio').classList.add('tab--active');
+
+            botones[0].classList.remove('tabs__button--active');
+            botones[1].classList.remove('tabs__button--active');
+
+            pestañas[0].classList.remove('tab--active');
+            pestañas[1].classList.remove('tab--active');
+        }
+    }
+   
+});
